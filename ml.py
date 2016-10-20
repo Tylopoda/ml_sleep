e6c8675bf5a0f6e5ec57be1b19e418fe0c45a60d
Tyler Lund <tylerlund@Tylers-MacBook-Air.local>
2016-10-20 17:30:20 -0400

    initial add of csv data file and code

---
+++ ml.py
@@ -0,0 +1,97 @@
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import cross_validation
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+names = ['total-food', 'last-feed-time', 'last-feed-amount', 'hours-of-sleep']
+dataset = pandas.read_csv("food.csv", names=names)
+
+# shape
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('hours-of-sleep').size())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:3]
+print X
+Y = array[:,3]
+Y = np.around(Y, decimals=0)
+print Y
+validation_size = 0.10
+seed = 7
+X_train, X_validation, Y_train, Y_validation = cross_validation.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+num_folds = 10
+num_instances = len(X_train)
+seed = 7
+scoring = 'accuracy'
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+	cv_results = cross_validation.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+# Make predictions on validation dataset
+svc = SVC()
+svc.fit(X_train, Y_train)
+predictions = svc.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+print(svc.predict([[28, 7, 6]]))
